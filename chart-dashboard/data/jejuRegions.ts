5e69970c723c06f338ef7c15fa89de1e152448df
Huiwon <71807924+hu1won@users.noreply.github.com>
2024-12-26 13:14:53 +0900

    jejuregion 경계선

---
+++ chart-dashboard/data/jejuRegions.ts
@@ -0,0 +1,23 @@
+export const jejuRegionsGeoJSON = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": { "name": "제주시" },
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[126.15, 33.45], [126.85, 33.45], [126.85, 33.6], [126.15, 33.6], [126.15, 33.45]]]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": { "name": "서귀포시" },
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[126.15, 33.2], [126.85, 33.2], [126.85, 33.45], [126.15, 33.45], [126.15, 33.2]]]
+        }
+      }
+    ]
+  };
+  
+  
